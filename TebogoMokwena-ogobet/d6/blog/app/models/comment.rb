590254d5f65ecfa1b3daa73fe1c08dd9768eb25f
Tebogo <ogobet>
2015-06-22 19:29:15 +0200

    Adding my first rails blog!

---
+++ TebogoMokwena-ogobet/d6/blog/app/models/comment.rb
@@ -0,0 +1,18 @@
+class Comment < ActiveRecord::Base
+  belongs_to :article
+end
+
+# class CreateComments < ActiveRecord::Migration
+#   def change
+#     create_table :comments do |t|
+#       t.string :commenter
+#       t.text :body
+ 
+#       # this line adds an integer column called `article_id`.
+#       t.references :article, index: true
+ 
+#       t.timestamps null: false
+#     end
+#     add_foreign_key :comments, :articles
+#   end
+# end
---
+++ TebogoMokwena-ogobet/d6/blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '318765aa095e818c925ee2b25c555e3e4ef60dfb397902587f881b73a4bc93b5efa9a8d21253dc1b1b0469223e2906b1fe0ce1c9d6ed9de246c61eaccf1865c3'
